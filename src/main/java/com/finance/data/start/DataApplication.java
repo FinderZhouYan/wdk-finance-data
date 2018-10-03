aebde3ca5532da6b61cc6e45bf5b9bab0d1be1d3
FinderZhouYan <17602199257@163.com>
2018-10-03 21:15:02 +0800

    修改启动类的目录

--- src/main/java/com/finance/data/start/DataApplication.java
+++ src/main/java/com/finance/data/start/DataApplication.java
@@ -1,4 +1,4 @@
-package com.finance.data;
+package com.finance.data.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
